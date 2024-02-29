0e506aff816954104c7a4f218f870781cd19c8a5
Molix228 <ideamesly123@gmail.com>
2024-03-01 01:43:37 +0200

    commit

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = 'uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+app.config['SECRET_KEY'] = 'wazxdesz21'
 db = SQLAlchemy(app)
 
 

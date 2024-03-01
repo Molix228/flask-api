c6141f396a7ddd9e3f45e5d6b073137608ee8dc0
Molix228 <ideamesly123@gmail.com>
2024-03-01 03:35:03 +0200

    commit

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
-from flask_wtf import FlaskForm, CSRFProtect
-from wtforms import StringField, FileField
 
 app = Flask(__name__)
-csrf = CSRFProtect(app)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cars.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -16,7 +13,6 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 app.config['SECRET_KEY'] = 'wazxdesz21'
 db = SQLAlchemy(app)
-
 
 class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,28 +28,11 @@ class Car(db.Model):
     photo = db.Column(db.String(100))
     description = db.Column(db.Text)
 
-
-class CarForm(FlaskForm):
-    brand = StringField('Brand')
-    model = StringField('Model')
-    type = StringField('Type')
-    axle = StringField('Axle')
-    year = StringField('Year')
-    price = StringField('Price')
-    color = StringField('Color')
-    weight = StringField('Weight')
-    mileage = StringField('Mileage')
-    photo = FileField('Photo')
-    description = StringField('Description')
-
-
 with app.app_context():
     db.create_all()
 
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
 
 app.config['MAX_CONTENT_LENGTH'] = MAX_CONTENT_LENGTH
 
@@ -78,50 +57,35 @@ def get_cars():
                 'description': car.description
             })
         return jsonify({'cars': car_list})
-
     except Exception as e:
         return jsonify({'error': 'Internal Server Error'}), 500
 
 @app.route('/api/cars', methods=['POST'])
 def add_car():
     try:
-        form = CarForm(request.form)
+        form = request.form
 
-        print("Form Data:")
-        for field in form:
-            print(f"{field.name}: {field.data}")
+        new_car = Car(
+            brand=form['brand'],
+            model=form['model'],
+            type=form['type'],
+            axle=form['axle'],
+            year=form['year'],
+            price=form['price'],
+            color=form['color'],
+            weight=form['weight'],
+            mileage=form['mileage'],
+            photo=form['photo'],
+            description=form['description']
+        )
 
-        if form.validate_on_submit():
-            file = request.files['photo']
+        db.session.add(new_car)
+        db.session.commit()
 
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-            new_car = Car(
-                brand=form.brand.data,
-                model=form.model.data,
-                type=form.type.data,
-                axle=form.axle.data,
-                year=form.year.data,
-                price=form.price.data,
-                color=form.color.data,
-                weight=form.weight.data,
-                mileage=form.mileage.data,
-                photo=filename,
-                description=form.description.data
-            )
-
-            db.session.add(new_car)
-            db.session.commit()
-
-            return jsonify({"message": "Car added successfully!"})
-        else:
-            return jsonify({"error": "Invalid form data"}), 400
-
+        return jsonify({"message": "Car added successfully!"})
     except Exception as e:
         db.session.rollback()
         return jsonify({"error": f"Internal Server Error: {e}"}), 500
-
 
 if __name__ == '__main__':
     app.run(debug=True)
